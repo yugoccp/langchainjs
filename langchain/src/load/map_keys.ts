04cad3a50f462343a656a438cc9d4380a59f0aa7
Tat Dat Duong <david@duong.cz>
2023-09-12 18:05:17 +0200

    Support nested paths for aliasing

--- langchain/src/load/map_keys.ts
+++ langchain/src/load/map_keys.ts
@@ -10,6 +10,38 @@ export interface SerializedKeyAlias {
   [key: string]: string;
 }
 
+function getPath(fields: SerializedFields, path: string): unknown {
+  const [last, ...rest] = path.split(".");
+  let current: SerializedFields = fields;
+  for (const part of rest.reverse()) {
+    if (!(part in current) || current === undefined) return undefined;
+    if (Array.isArray(current[part]))
+      throw new Error("Aliasing arrays is not supported");
+    current = current[part];
+  }
+  return current[last];
+}
+
+function setPath(fields: SerializedFields, path: string, value: unknown) {
+  const [last, ...rest] = path.split(".").reverse();
+  let current: SerializedFields = fields;
+  for (const part of rest.reverse()) {
+    if (!(part in current) || current[part] === undefined) current[part] = {};
+    current = current[part];
+  }
+  current[last] = value;
+}
+
+function deletePath(fields: SerializedFields, path: string) {
+  const [last, ...rest] = path.split(".").reverse();
+  let current: SerializedFields = fields;
+  for (const part of rest.reverse()) {
+    if (!(part in current) || current[part] === undefined) return;
+    current = current[part];
+  }
+  delete current[last];
+}
+
 export function keyToJson(key: string, map?: SerializedKeyAlias): string {
   return map?.[key] || snakeCase(key);
 }
@@ -27,8 +59,19 @@ export function mapKeys(
 
   for (const key in fields) {
     if (Object.hasOwn(fields, key)) {
-      mapped[mapper(key, map)] = fields[key];
+      const targetKey = mapper(key, map);
+      if (targetKey.includes(".") || key.includes(".")) continue;
+      mapped[targetKey] = fields[key];
     }
+  }
+
+  const nestedMap = Object.entries(map ?? {}).filter((paths) =>
+    paths.some((path) => path.includes("."))
+  );
+  for (const [source, target] of nestedMap) {
+    const value = getPath(fields, source);
+    setPath(mapped, target, value);
+    deletePath(mapped, source);
   }
 
   return mapped;
